817a72166cc1be21cf06069474521d8593aea300
Zero3141 <hkiesel20@gmail.com>
2020-12-31 18:12:15 +0100

    Refactoring for Issue #1

--- okhttphijack.js
+++ okhttphijack.js
@@ -15,29 +15,29 @@ setTimeout(function () {
         console.log("");
 
         // Inject custom certificate
-        var CertificateFactory = Java.use("java.security.cert.CertificateFactory");
-        var FileInputStream = Java.use("java.io.FileInputStream");
-        var BufferedInputStream = Java.use("java.io.BufferedInputStream");
-        var KeyStore = Java.use("java.security.KeyStore");
-        var TrustManagerFactory = Java.use("javax.net.ssl.TrustManagerFactory");
-        var SSLContext = Java.use("javax.net.ssl.SSLContext");
+        const CertificateFactory = Java.use("java.security.cert.CertificateFactory");
+        const FileInputStream = Java.use("java.io.FileInputStream");
+        const BufferedInputStream = Java.use("java.io.BufferedInputStream");
+        const KeyStore = Java.use("java.security.KeyStore");
+        const TrustManagerFactory = Java.use("javax.net.ssl.TrustManagerFactory");
+        const SSLContext = Java.use("javax.net.ssl.SSLContext");
 
-        var cf = CertificateFactory.getInstance("X.509");
+        const cf = CertificateFactory.getInstance("X.509");
         try {
-            var fileInputStream = FileInputStream.$new("/data/local/tmp/root.cer");
+            const fileInputStream = FileInputStream.$new("/data/local/tmp/root.cer");
         }
         catch(err) {
             console.log("[-] " + err);
         }
-        var bufferedInputStream = BufferedInputStream.$new(fileInputStream);
-        var ca = cf.generateCertificate(bufferedInputStream);
+        const bufferedInputStream = BufferedInputStream.$new(fileInputStream);
+        const ca = cf.generateCertificate(bufferedInputStream);
         bufferedInputStream.close();
-        var keyStoreType = KeyStore.getDefaultType();
-        var keyStore = KeyStore.getInstance(keyStoreType);
+        const keyStoreType = KeyStore.getDefaultType();
+        const keyStore = KeyStore.getInstance(keyStoreType);
         keyStore.load(null, null);
         keyStore.setCertificateEntry("ca", ca);
-        var tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-        var tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        const tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        const tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
         tmf.init(keyStore);
         SSLContext.init.overload("[Ljavax.net.ssl.KeyManager;", "[Ljavax.net.ssl.TrustManager;", "java.security.SecureRandom").implementation = function(a,b,c) {
             console.log("[+] SSLContext overloaded")
@@ -46,11 +46,11 @@ setTimeout(function () {
 
 
         // Hijacking CertificatePinner.Builder.build() by returning empty CertificatePinner
-        var LinkedHashSet = Java.use("java.util.LinkedHashSet")
-        var CertificatePinner = Java.use("okhttp3.CertificatePinner")
-        var hashSet = LinkedHashSet.$new();
-        var certPinner = CertificatePinner.$new(hashSet, null);
-        var Builder = Java.use("okhttp3.CertificatePinner$Builder");
+        const LinkedHashSet = Java.use("java.util.LinkedHashSet")
+        const CertificatePinner = Java.use("okhttp3.CertificatePinner")
+        const hashSet = LinkedHashSet.$new();
+        const certPinner = CertificatePinner.$new(hashSet, null);
+        const Builder = Java.use("okhttp3.CertificatePinner$Builder");
         Builder.build.overload().implementation = function() {
             console.log("[+] CertificatePinner overloaded")
             return certPinner;
